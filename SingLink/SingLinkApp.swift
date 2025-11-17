a8d143206b29c6bba238496741f9b17baf949c26
Reinner Steven Daza Leiva <reinner.leiva@outlook.com>
2025-11-17 01:48:43 -0500

    Initial Commit

---
+++ SingLink/SingLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SingLinkApp.swift
+//  SingLink
+//
+//  Created by Reinner Steven Daza Leiva on 17/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SingLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
